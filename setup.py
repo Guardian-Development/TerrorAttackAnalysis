e26a1af43986f78f0dc93a52975b4717c35392af
Joe Honour <joe.honour@bjss.com>
2017-01-01 13:24:04 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Terror Attack Analysis',
+    version='0.0.1',
+    description='Data anlysis on the Gloval Terror Database',
+    long_description='N/A',
+    author='Joe Honour',
+    author_email='joehonour@hotmail.co.uk',
+    url='https://github.com/Guardian-Development/TerrorAttackAnalysis',
+    license='',
+    packages=find_packages()
+)
